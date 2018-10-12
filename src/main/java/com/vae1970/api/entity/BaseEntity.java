4d43eb023083f980f91abb3e9afe3ead06051592
vae1970 <496075305@qq.com>
2018-10-12 23:53:11 +0800

    chore: add baseEntity

---
+++ src/main/java/com/vae1970/api/entity/BaseEntity.java
@@ -0,0 +1,44 @@
+package com.vae1970.api.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * base entity
+ *
+ * @author vae
+ */
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "BIGINT(20) UNSIGNED")
+    protected Long id;
+
+    @CreationTimestamp
+    @Column(updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Date createdAt;
+
+    @Column(updatable = false, columnDefinition = "BIGINT(20) UNSIGNED")
+    private Long createdBy;
+
+    @UpdateTimestamp
+    @Column(columnDefinition = "TIMESTAMP NULL DEFAULT NULL")
+    private Date updatedAt;
+
+    @Column(columnDefinition = "BIGINT(20) UNSIGNED")
+    private Long updatedBy;
+
+    @Version
+    @Column(columnDefinition = "INT(10) UNSIGNED DEFAULT 0")
+    private Integer version;
+
+}
